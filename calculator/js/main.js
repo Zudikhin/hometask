0190222c0acda90cf795a1406b34b66271122a9a
Yuriy Zudikhin <uzudihin@gmail.com>
2019-12-16 01:07:54 +0300

    lol

--- calculator/js/main.js
+++ calculator/js/main.js
@@ -3,8 +3,6 @@ let calculator = document.querySelector(".calculator");
 let calculatorMain = document.querySelector(".calculator__main");
 let calculatorResult = document.querySelector(".calculator__result");
 let calculatorKeyboard = document.querySelector(".calculator__keyboard");
-let numbers = document.querySelectorAll(".numbers");
-let operations = document.querySelectorAll(".operations");
 let percent = document.querySelector(".percent");
 let ce = document.querySelector("#ce");
 let c = document.querySelector("#c");
@@ -15,22 +13,41 @@ let root = document.querySelector("#root");
 let changePlusMinus = document.querySelector("#change_plus_minus");
 let coma = document.querySelector("#coma");
 let equal = document.querySelector("#equal");
+let plus = document.querySelector("#plus");
+let times = document.querySelector("#times");
+let divide = document.querySelector("#divide");
+let minus = document.querySelector("#minus");
 
-for (let i = 0; i < numbers.length; i++) {
-  let number = numbers[i];
-  number.addEventListener("click", function(e) {
-    console.log("number");
-  });
+let seven = document.querySelector("#seven");
+let eight = document.querySelector("#eight");
+let nine = document.querySelector("#nine");
+let four = document.querySelector("#four");
+let five = document.querySelector("#five");
+let six = document.querySelector("#six");
+let one = document.querySelector("#one");
+let two = document.querySelector("#two");
+let three = document.querySelector("#three");
+let zero = document.querySelector("#zero");
+
+seven.addEventListener("click", addNumber);
+eight.addEventListener("click", addNumber);
+nine.addEventListener("click", addNumber);
+four.addEventListener("click", addNumber);
+five.addEventListener("click", addNumber);
+six.addEventListener("click", addNumber);
+one.addEventListener("click", addNumber);
+two.addEventListener("click", addNumber);
+three.addEventListener("click", addNumber);
+zero.addEventListener("click", addNumber);
+
+function addNumber(event) {
+  calculatorResult.innerHTML += event.target.innerHTML;
 }
 
-for (let i = 0; i < operations.length; i++) {
-  let operation = operations[i];
-  operation.addEventListener("click", function(e) {
-    operate(e.target.textContent);
-  });
+plus.addEventListener("click", operatePlus);
+
+function operatePlus() {
+  let num = calculatorResult.innerHTML;
+  let znak = calculatorResult.innerHTML = "+";
+  console.log(num);
 }
-
-equal.addEventListener("click", function(e) {});
-
-let input = document.querySelector("#input");
-console.log(input.value);
